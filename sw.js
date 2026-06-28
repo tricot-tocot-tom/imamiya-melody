e980a420e8c49de57dab9d487185696f53eb5a50
tricot <om.mani.padme.hum.tricot.tocot@gmail.com>
2026-06-28 12:31:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-const CACHE='imamiya-melody-2go-v1';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='imamiya-melody-henshubu-v2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','profile-camera.jpg','profile-cat.jpg'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
