6472d6998e1ff6306d7162c57f1c0549fa471381
tricot <om.mani.padme.hum.tricot.tocot@gmail.com>
2026-06-28 21:37:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'imamiya-melody-v2-9-2';
+const CACHE_NAME = 'imamiya-melody-v2-9-3';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./profile-cat.jpg','./profile-camera.jpg','./jszip.min.js'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
