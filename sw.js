4cf5808309d77a6a2d210c7bb51fbf50ec635d56
tricot <om.mani.padme.hum.tricot.tocot@gmail.com>
2026-06-30 19:24:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='imamiya-melody-v5-0';
+const CACHE='imamiya-melody-v5-3';
 self.addEventListener('install', e=>{self.skipWaiting();});
 self.addEventListener('activate', e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch', e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+self.addEventListener('fetch', e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))) });
