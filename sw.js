5f5f16072a0e8311f985af4219de78e0f4cee511
tricot <om.mani.padme.hum.tricot.tocot@gmail.com>
2026-06-30 17:59:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,4 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
+const CACHE_NAME='imamiya-melody-poco-v4-2';
+self.addEventListener('install',event=>{self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); self.clients.claim();});
+self.addEventListener('fetch',event=>{});
